5bc36c28f26093b7b6f48fb70761ecc087edf7c5
marin259 <157164854+marin259@users.noreply.github.com>
2026-09-10 12:38:12 -0400

    cambio

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="p-8 text-white">
+      <h1 className="text-3xl font-bold">Blog de la Agencia</h1>
+    </div>
+  );
+}
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+  return (
+    <div className="p-8 text-white">
+      <h1 className="text-3xl font-bold">Portafolio de Tatuajes</h1>
+    </div>
+  );
+}
